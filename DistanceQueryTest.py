54213ff5d8fd3d8743eaf7b6ae0db0225c4a2f86
Jesse Rowley <rowleyj@mcmaster.ca>
2022-02-26 14:07:23 -0500

    Update DistanceQueryTest.py

--- DistanceQueryTest.py
+++ DistanceQueryTest.py
@@ -6,18 +6,33 @@ import sys
 
 ser = serial.Serial('/dev/serial0', 921600, timeout = 2)
 
-ser.write(bytearray.fromhex('57 10 FF FF 00 FF FF 63'))
+
+# while True:
+# 	TOF_data=()
+	
+	
+# 	if ser.in_waiting >= 6:
+# 		print(ser.in_waiting)
+# 		for i in range(0,ser.in_waiting):
+# 			print(ord(ser.read(1)))
+# 			#print(ser.read(1))
+		
+# 		ser.close()
+# 		break
+
+writeToId = { '00', '01', '02'}
+idx = 0
 
 while True:
-	TOF_data=()
-	
+	print('top of loop', 'writing to: '+writeToId[idx])
+	ser.write(bytearray.fromhex('57 10 FF FF '+writeToId[idx]+' FF FF 63'))
+
+	while(ser.in_waiting < 6) print('waiting...')
 	
 	if ser.in_waiting >= 6:
-		print(ser.in_waiting)
-		for i in range(0,ser.in_waiting):
+		for i in range(0, ser.in_waiting):
 			print(ord(ser.read(1)))
-			#print(ser.read(1))
-		
-		ser.close()
-		break
 
+	# alter id to write to next in queue
+	idx = idx + 1
+	if(idx > len(writeToId)) idx = 0
